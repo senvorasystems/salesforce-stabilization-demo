1cf2775fc1cf388e02c9844d70f500d47530886f
Angel Dario Paguay Guevara <angel@senvorasystems.com>
2026-09-01 19:55:01 +0200

    fix: use Jest globals in Code Analyzer

--- eslint.config.js
+++ eslint.config.js
@@ -28,7 +28,7 @@ module.exports = defineConfig([
     languageOptions: {
       globals: {
         ...globals.node,
-        ...jestPlugin.environments.globals.globals
+        ...globals.jest
       }
     }
   },
